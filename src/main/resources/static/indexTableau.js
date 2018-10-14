994134dc5e0e46553c3e4244b28314d5acb6c5a5
yaya <papmalle@gmail.com>
2018-10-14 20:20:32 +0200

    Update src/main/resources/static/indexTableau.js
    fonction unshift ()et push()

--- src/main/resources/static/indexTableau.js
+++ src/main/resources/static/indexTableau.js
@@ -1,1 +1,5 @@
-var array = [" test", 1,2,3,"hello",9,6];
+var array = [" test", 1, 2, 3, "hello", 9, 6];
+array.push("test2", "test3", 5, 6);// La méthode push()permet d'ajouter un ou plusieurs items à la FIN du tableau
+// La méthode unshift()permet d'ajouter un ou plusieurs items au DEBUT du tableau
+array.unshift("même que push() sauf que les valeurs sont ajoutés au debut du tableau");
+alert(array);
